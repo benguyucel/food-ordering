84f489d419f3f88630c4178ad2d68c2309edac91
benguyucel <yucelbengu8@gmail.com>
2023-01-12 13:30:31 +0300

    added middleware

--- middleware.ts
+++ middleware.ts
@@ -2,34 +2,32 @@ import { getToken } from 'next-auth/jwt';
 import { NextRequest, NextResponse } from 'next/server';
 
 import { withAuth } from "next-auth/middleware"
-import { includes } from 'lodash';
-
 
 const onylAdminCanReach = [
     '/admin',
     '/api/admin',
 ]
-const isAdminRoute = (pathname) => {
+const isAdminRoute = (pathname: string) => {
     return onylAdminCanReach.some(route => pathname.startsWith(route))
 }
 
 export default withAuth(
-    async function middleware(req) {
+    async function middleware(req: NextRequest) {
 
-        const token = await getToken({ req })
+        const token: any = await getToken({ req })
         const { pathname } = req.nextUrl;
         //if token does not exist retun home
         if (!token) return NextResponse.redirect(new URL('/', `${process.env.NEXT_API_BASE_URL}`))
         //Only admin can reach
-        if (isAdminRoute(pathname) && !includes(['admin'], token.role)) {
+        if (isAdminRoute(pathname) && !['admin'].includes(token.role)) {
             return NextResponse.redirect(new URL('/', `${process.env.NEXT_API_BASE_URL}`))
         }
         if (pathname.startsWith('/api/users')) {
             let userId = pathname.split('/')[3]
-            if (typeof userId === "undefined" && !includes(["admin"], token.role)) {
+            if (typeof userId === "undefined" && !['admin'].includes(token.role)) {
                 return NextResponse.redirect(new URL('/', `${process.env.NEXT_API_BASE_URL}`))
             }
-            if (userId !== token.uid || !includes(['admin', 'user'], token.role)) {
+            if (userId !== token.uid || !['admin', 'user'].includes(token.role)) {
                 return NextResponse.redirect(new URL('/', `${process.env.NEXT_API_BASE_URL}`))
             }
         }
